ddc9b27e5f52211f3ce35ab0051a3cb52d404099
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-08-09 14:12:26 +0600

    .

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import CounterPage from './pages/CounterPage';
+import TodoPage from './pages/TodoPage';
+import "./assets/css/app.css";
+import "./assets/css/style.css";
+//import CounterPage from './pages/CounterPage';
+
 const App = () => {
   return (
     <div>
-      <CounterPage />
+      <TodoPage />
     </div>
   );
 };
--- counter-app/src/redux/store/store.js
+++ counter-app/src/redux/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../state/counter/counterSlice"; // You need to import the counterReducer
+import todoReducer from "../state/todo/todoSlice"; // You need to import the todoReducer
+
 
 export default configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        todo: todoReducer
     }
 });
